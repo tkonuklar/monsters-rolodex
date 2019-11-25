c9f1d49b9e8fb494f26e61fa4ff9eb47c34ab1bb
tugcekonuklar <tugce.konuklar@infinitec.solutions>
2019-11-25 17:40:57 +0100

    fist commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters : [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>{ this.state.stringsTugce }</p>
+          <button onClick={() => this.setState({stringsTugce: 'Hello Serkan'})}>Change Text</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
