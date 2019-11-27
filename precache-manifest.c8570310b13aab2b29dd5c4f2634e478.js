7c425dde86e479c45fa7cc27c7f4cd3d390ae29d
tugcekonuklar <tugce.konuklar@infinitec.solutions>
2019-11-27 20:50:00 +0100

    Updates

---
+++ precache-manifest.c8570310b13aab2b29dd5c4f2634e478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36201e1a8d8caad1c18462c2aa90c3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f2d7777efde6c3bd498",
+    "url": "/monsters-rolodex/static/css/main.632215f3.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "9f2d7777efde6c3bd498",
+    "url": "/monsters-rolodex/static/js/main.0360d92a.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
